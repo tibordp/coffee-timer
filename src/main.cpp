17a05df51dbe1c7c154f9e647f050e77db205aef
Tibor Djurica Potpara <tibor.djurica@ojdip.net>
2021-05-08 14:35:18 +0100

    Change buzzing pattern a bit

--- src/main.cpp
+++ src/main.cpp
@@ -28,13 +28,24 @@
 #define EDIT_TIMEOUT 10000
 
 const uint8_t digits[] = {
-    0b11111100, 0b01100000, 0b11011010, 0b11110010, 0b01100110,
-    0b10110110, 0b10111110, 0b11100000, 0b11111110, 0b11110110,
+    0b11111100,
+    0b01100000,
+    0b11011010,
+    0b11110010,
+    0b01100110,
+    0b10110110,
+    0b10111110,
+    0b11100000,
+    0b11111110,
+    0b11110110,
 };
 
-void shiftSegments(bool buzzer, uint8_t segments, uint8_t groundMask) {
-  for (uint8_t i = 0; i < 16; i++) {
-    switch (i) {
+void shiftSegments(bool buzzer, uint8_t segments, uint8_t groundMask)
+{
+  for (uint8_t i = 0; i < 16; i++)
+  {
+    switch (i)
+    {
     case 2:
     case 3:
       digitalWrite(SHIFT_DATA, !(groundMask & (1 << (i - 2))));
@@ -66,7 +77,8 @@ void shiftSegments(bool buzzer, uint8_t segments, uint8_t groundMask) {
   digitalWrite(SHIFT_LATCH, LOW);
 }
 
-void writeNumber(bool buzzer, uint16_t number) {
+void writeNumber(bool buzzer, uint16_t number)
+{
   shiftSegments(buzzer, digits[number % 10], 0b0001);
   delayMicroseconds(200);
   number /= 10;
@@ -80,7 +92,8 @@ void writeNumber(bool buzzer, uint16_t number) {
   delayMicroseconds(200);
 }
 
-void writeDuration(bool buzzer, uint32_t milliseconds, uint8_t mask) {
+void writeDuration(bool buzzer, uint32_t milliseconds, uint8_t mask)
+{
   int seconds = ((milliseconds + 500) / 1000) % 60; // round up
   int minutes = ((milliseconds + 500) / 60000);
 
@@ -99,7 +112,8 @@ void writeDuration(bool buzzer, uint32_t milliseconds, uint8_t mask) {
 
 void turnOffDisplay(bool buzzer) { shiftSegments(buzzer, 0, 0b0000); }
 
-void setup() {
+void setup()
+{
   pinMode(SHIFT_DATA, OUTPUT);
   pinMode(SHIFT_LATCH, OUTPUT);
   pinMode(SHIFT_CLK, OUTPUT);
@@ -113,7 +127,8 @@ void setup() {
   ADCSRA &= ~_BV(ADEN);
 }
 
-ISR(PCINT0_vect) {
+ISR(PCINT0_vect)
+{
   // Empty ISR, we only need it to be able to wake up from sleep
   // on button press.
 }
@@ -124,7 +139,7 @@ uint8_t state = 0;
 uint32_t startedAt = 0;
 uint32_t lastInteraction = 0;
 
-uint32_t coffeeTime[2] = {180000, 240000};
+uint32_t coffeeTime[2] = {240000, 180000};
 uint8_t editMinutes = 0;
 uint8_t editSeconds = 0;
 
@@ -134,134 +149,167 @@ bool shortBeep = false;
 uint32_t shortBeepStart = 0;
 uint32_t sleepTimeoutStart = 0;
 
-#define CHECK_BUTTON_CUSTOM(shouldBePressed, timeout)                          \
-  (buttonPressed ^ (shouldBePressed) &&                                        \
-   currentTime - lastInteraction > timeout &&                                  \
+#define CHECK_BUTTON_CUSTOM(shouldBePressed, timeout) \
+  (buttonPressed ^ (shouldBePressed) &&               \
+   currentTime - lastInteraction > timeout &&         \
    ((lastInteraction = currentTime), true))
 #define BUTTON_UP CHECK_BUTTON_CUSTOM(false, BUTTON_DEBOUNCE_MS)
 #define BUTTON_DOWN CHECK_BUTTON_CUSTOM(true, BUTTON_DEBOUNCE_MS)
 
 #define EDITED_TIME (((uint32_t)editMinutes * 60 + editSeconds) * 1000)
 
-#define SHORT_BEEP                                                             \
-  do {                                                                         \
-    shortBeep = true;                                                          \
-    shortBeepStart = currentTime;                                              \
+#define SHORT_BEEP                \
+  do                              \
+  {                               \
+    shortBeep = true;             \
+    shortBeepStart = currentTime; \
   } while (false)
 
-void goToSleep() {
+void goToSleep()
+{
   set_sleep_mode(SLEEP_MODE_PWR_DOWN);
   sleep_mode();
 }
 
-void loop() {
+void loop()
+{
   uint32_t currentTime = millis();
   uint8_t button1Pressed = digitalRead(BUTTON_1);
   uint8_t button2Pressed = digitalRead(BUTTON_2);
   uint8_t buttonPressed = button1Pressed && button2Pressed;
 
   // State transitions
-  switch (state) {
+  switch (state)
+  {
   case STATE_DEFAULT_1:
-    if (BUTTON_UP) {
+    if (BUTTON_UP)
+    {
       state = STATE_DEFAULT;
       sleepTimeoutStart = currentTime;
     }
     break;
   case STATE_DEFAULT:
-    if (BUTTON_DOWN) {
+    if (BUTTON_DOWN)
+    {
       selectedButton = !button2Pressed ? 0 : 1;
       state = STATE_RUNNING_1;
     }
     break;
   case STATE_RUNNING_1:
-    if (BUTTON_UP) {
+    if (BUTTON_UP)
+    {
       SHORT_BEEP;
       startedAt = currentTime;
       state = STATE_RUNNING;
-    } else if (CHECK_BUTTON_CUSTOM(true, 1000)) {
+    }
+    else if (CHECK_BUTTON_CUSTOM(true, 1000))
+    {
       editSeconds = 0;
       editMinutes = 0;
       state = STATE_SETTING_MINUTES_1;
     }
     break;
   case STATE_SETTING_MINUTES_1:
-    if (BUTTON_UP) {
+    if (BUTTON_UP)
+    {
       state = STATE_SETTING_MINUTES;
     }
     break;
   case STATE_SETTING_MINUTES:
-    if (BUTTON_DOWN) {
+    if (BUTTON_DOWN)
+    {
       state = STATE_SETTING_MINUTES_2;
-    } else if (currentTime - lastInteraction > EDIT_TIMEOUT) {
+    }
+    else if (currentTime - lastInteraction > EDIT_TIMEOUT)
+    {
       state = STATE_DEFAULT;
       sleepTimeoutStart = currentTime;
     }
     break;
   case STATE_SETTING_MINUTES_2:
-    if (BUTTON_UP) {
+    if (BUTTON_UP)
+    {
       SHORT_BEEP;
       editMinutes = (editMinutes + 1) % 100;
       state = STATE_SETTING_MINUTES;
-    } else if (CHECK_BUTTON_CUSTOM(true, EDIT_START_THRESHOLD)) {
+    }
+    else if (CHECK_BUTTON_CUSTOM(true, EDIT_START_THRESHOLD))
+    {
       SHORT_BEEP;
       state = STATE_SETTING_SECONDS_1;
     }
     break;
   case STATE_SETTING_SECONDS_1:
-    if (BUTTON_UP) {
+    if (BUTTON_UP)
+    {
       state = STATE_SETTING_SECONDS;
     }
     break;
   case STATE_SETTING_SECONDS:
-    if (BUTTON_DOWN) {
+    if (BUTTON_DOWN)
+    {
       state = STATE_SETTING_SECONDS_2;
-    } else if (currentTime - lastInteraction > EDIT_TIMEOUT) {
+    }
+    else if (currentTime - lastInteraction > EDIT_TIMEOUT)
+    {
       state = STATE_DEFAULT;
       sleepTimeoutStart = currentTime;
     }
     break;
   case STATE_SETTING_SECONDS_2:
-    if (BUTTON_UP) {
+    if (BUTTON_UP)
+    {
       SHORT_BEEP;
       editSeconds = (editSeconds + 1) % 60;
       state = STATE_SETTING_SECONDS;
-    } else if (CHECK_BUTTON_CUSTOM(true, EDIT_START_THRESHOLD)) {
+    }
+    else if (CHECK_BUTTON_CUSTOM(true, EDIT_START_THRESHOLD))
+    {
       SHORT_BEEP;
       coffeeTime[selectedButton] = EDITED_TIME;
       state = STATE_DEFAULT_1;
     }
     break;
   case STATE_RUNNING:
-    if (BUTTON_DOWN) {
+    if (BUTTON_DOWN)
+    {
       state = STATE_DEFAULT_1;
-    } else if (currentTime - startedAt > coffeeTime[selectedButton]) {
+    }
+    else if (currentTime - startedAt > coffeeTime[selectedButton])
+    {
       state = STATE_BUZZING;
       startedAt = currentTime;
     }
     break;
   case STATE_BUZZING:
-    if (BUTTON_DOWN) {
+    if (BUTTON_DOWN)
+    {
       state = STATE_DEFAULT_1;
-    } else if (currentTime - startedAt > 10000) {
+    }
+    else if (currentTime - startedAt > 30000)
+    {
       state = STATE_DEFAULT;
       sleepTimeoutStart = currentTime;
     }
     break;
   }
 
-  if (shortBeep) {
+  if (shortBeep)
+  {
     buzzing = true;
-    if (currentTime - shortBeepStart > 100) {
+    if (currentTime - shortBeepStart > 60)
+    {
       buzzing = false;
     }
   }
 
   // Display
-  switch (state) {
+  switch (state)
+  {
   case STATE_DEFAULT:
   case STATE_DEFAULT_1:
-    if (!shortBeep) {
+    if (!shortBeep)
+    {
       buzzing = false;
     }
     turnOffDisplay(buzzing);
@@ -287,19 +335,25 @@ void loop() {
                   0b1111);
     break;
   case STATE_BUZZING:
-    if ((currentTime / 150) % 2) {
+    int bucket = (currentTime - startedAt) % 1000;
+    int segment = bucket / 60;
+    if ((segment < 8) && (segment % 2 == 0))
+    {
       buzzing = true;
       writeDuration(buzzing, 0, 0b1111);
-    } else {
+    }
+    else
+    {
       buzzing = false;
       turnOffDisplay(buzzing);
     }
     break;
   }
 
-  // We do this at the end to make sure the display is blank and the 
+  // We do this at the end to make sure the display is blank and the
   // buzzer is disabled.
-  if (state == STATE_DEFAULT && currentTime - sleepTimeoutStart > 1000) {
+  if (state == STATE_DEFAULT && currentTime - sleepTimeoutStart > 1000)
+  {
     // Save battery by disabling the clock after one second of idle
     goToSleep();
     sleepTimeoutStart = currentTime;
